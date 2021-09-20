28be166be557bb10792f41589f71c9aee4188fad
Shane-Gray394 <86175936+Shane-Gray394@users.noreply.github.com>
2021-09-20 16:02:03 -0400

    fleshed out addplant

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import AddPlant from "./components/Add Plant/AddPlant";
 
 function App() {
-  return <div className="App">Water my plants</div>;
+  return (
+    <div className="App">
+      Water my plants
+      <AddPlant />
+    </div>
+  );
 }
 
 export default App;
