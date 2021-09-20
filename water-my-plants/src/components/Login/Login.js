92bc6e29721ddd4d0eb37efa9cbd50f94d69abb3
Leo Hays <starryhornet@gmail.com>
2021-09-20 15:16:41 -0500

    login form part 1

--- water-my-plants/src/components/Login/Login.js
+++ water-my-plants/src/components/Login/Login.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function Login(props) {
+   const [cred, setCred] = useState({
+       username: '',
+       password: '',
+       phone_number: 0
+   })
+
+   const handleChange = e => {
+       setCred({
+           ...cred,
+           [e.target.name]: e.target.value
+       })
+   }
+   const login = e => {
+       e.preventDefault();
+       axios.post('', cred)
+       .then(res => {
+           localStorage.setItem("token", res.data.token);
+           localStorage.setItem("phone_number", res.data.phone_number);
+           localStorage.setItem("username", res.data.username);
+           props.history.push('/landing');
+       })
+       .catch(err => {
+           console.error(err);
+       })
+   }
+
     return (
         <div>
-            Login form here
+           <h3>Login here</h3>
+            <form onSubmit={login}>
+                <input
+                   type='text'
+                   name='usename'
+                   value={cred.username}
+                   onChange={handleChange}
+                />
+                <input
+                   type="password"
+                   name="password"
+                   value={cred.password}
+                   onChange={handleChange}
+                />
+                <input
+                   type='number'
+                   name="phone_number"
+                   value={cred.phone_number}
+                   onChange={handleChange}
+                />
+                <button>Log in</button>
+            </form>
         </div>
     );
 }
