ef7eabe19f4943e0d8293684637170c28efdcbe4
Leo Hays <starryhornet@gmail.com>
2021-09-21 11:26:00 -0500

    links in nav and typo

--- water-my-plants/src/components/Header/Header.js
+++ water-my-plants/src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const token = localStorage.getItem("token");
     return (
         <div style={{border: "3px solid red"}}>
-            Header Portion
+            <h1>Title</h1>
+            <div className='nav-bar'>
+               
+                <Link to='/login' className="btn btn-click">Login</Link>
+
+                <Link to='/sign-up' className="btn btn-click">Sign Up</Link>
+
+                <Link to='/add-plant' className="btn btn-click">Plants</Link>
+
+                <Link to='/logout' className="btn btn-click">Logout</Link>
+
+            </div>
         </div>
     );
 };
