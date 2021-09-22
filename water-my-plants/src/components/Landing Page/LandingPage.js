69cb019fa18b761affeb09cd2a38fa1588b656d4
Stuart Darsey <stuart.darsey@gmail.com>
2021-09-22 13:33:02 -0400

    basic header and landing built

--- water-my-plants/src/components/Landing Page/LandingPage.js
+++ water-my-plants/src/components/Landing Page/LandingPage.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 import { 
     StyledButton
 } from './../../theme/Theme';
 
 import {
-    StyledLandingContainer
+    StyledLandingContainer,
+    StyledIntroContainer,
+    StyledSpan
 } from './LandingPageStyles';
+
+const landingImage = 'https://images.unsplash.com/photo-1487798452839-c748a707a6b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80'
 
 const LandingPage = () => {
     return (
         <StyledLandingContainer>
-            Landing page
-            <StyledButton>Home page!</StyledButton>
+
+            <StyledIntroContainer>
+                <h3>Introducing<br/><StyledSpan>Garden of Eden</StyledSpan></h3>
+                <StyledButton><Link to='/login'>Login</Link></StyledButton>
+                <StyledButton><Link to='sign-up'>Sign Up</Link></StyledButton>
+            </StyledIntroContainer>
+            
+
+            <img src={landingImage}/>
         </StyledLandingContainer>
     );
 };
--- water-my-plants/src/components/Landing Page/LandingPageStyles.js
+++ water-my-plants/src/components/Landing Page/LandingPageStyles.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
 export const StyledLandingContainer = styled.div`
-    border: 3px solid ${props => props.theme.colors.mainBrown};
+    /* border: 3px solid ${props => props.theme.colors.mainBrown}; */
     font-size: ${props => props.theme.fontSizes.large};
+    display: flex;
+    justify-content:space-around;
+`
+
+export const StyledIntroContainer = styled.div`
+    display: flex column;
+    /* border: 5px solid green; */
+`
+
+export const StyledSpan = styled.span`
+    color: ${props => props.theme.colors.mainGreen};
 `
