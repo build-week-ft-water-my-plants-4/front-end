166a8b81c5105e1c77c7c61c9ad71627dd105598
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2021-09-23 12:27:16 -0700

    added style to plantcard.js and layout changes to the dashboard

--- water-my-plants/src/components/Dashboard/DashboardStyles.js
+++ water-my-plants/src/components/Dashboard/DashboardStyles.js
@@ -9,8 +9,8 @@ export const StyledDash = styled.div`
     align-items: center;
     box-shadow: 6px 4px 2px 2px;
     margin: 2%;
-    margin-left: 30%;
-    margin-right: 30%;
+    margin-left: 20%;
+    margin-right: 20%;
     border-radius: 10px;
     height: 60px;
     color: lightgreen;
@@ -23,3 +23,17 @@ export const StyledDash = styled.div`
     align-items: center;
 }
 `
+
+// styling for the plant card
+export const StyledPlantCard=styled.div`
+display:flex;
+background-color: #e5e5e5;
+margin: 3% 2%;
+padding-right:4%;
+border-radius:25px;
+`
+export const StyledPlantCardDiv=styled.div`
+width:100%;
+border:1px solid red;
+`
+// styling end here
--- water-my-plants/src/components/Dashboard/PlantCard.js
+++ water-my-plants/src/components/Dashboard/PlantCard.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { AddPlantButton } from "../Add Plant/AddPlantStyles";
 import { Link } from 'react-router-dom'
+import {StyledPlantCard,StyledPlantCardDiv} from "./DashboardStyles"
+
 const Plant = (props) => {
     const { plant } = props;
     return(
-       <div>
-        <div className="plant-card">
+       <StyledPlantCard>
+        <StyledPlantCardDiv className="plant-card">
             <h5>{plant.nickname}</h5>
             <h5>{plant.species}</h5>
             <h5>{plant.h2o_frequency}</h5>
-        </div>
+            
+        </StyledPlantCardDiv>
         <div className='edit-btn'>
         {/* need to change link later when route is setup */}
         <Link to="/edit-plant"><AddPlantButton>Edit Plant</AddPlantButton></Link>
         </div>
-       </div>
+       </StyledPlantCard>
     )
 } 
 
